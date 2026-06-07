bc66984ed681151961c2aff7d28d85d7b9143cfd
Themecus <127447399+Themecus@users.noreply.github.com>
2026-06-06 20:21:12 -0400

    funcion Antigüedad_en_club_miembro
    cree la funcion asignada

--- scripts/rubrica2_1/functions.sql
+++ scripts/rubrica2_1/functions.sql
@@ -1,1 +1,8 @@
 --insertar aquí el code de las funciones
+
+CREATE OR REPLACE FUNCTION Antigüedad_en_club_miembro (v_fecha DATE) 
+RETURN NUMBER IS 
+BEGIN 
+    -- Se calcula la diferencia en días y se redondea al dividir entre los días del año.
+    RETURN (ROUND(((SYSDATE - v_fecha) / 365), 0)); 
+END;
