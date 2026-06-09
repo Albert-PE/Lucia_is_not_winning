1e1e580e0d08c5aa9fb463ff0a3570dcfb5c1f29
Alberto Rodríguez Villanueva <135548602+Albert-PE@users.noreply.github.com>
2026-06-08 22:36:12 -0400

    Correcciones Function
    todo minuscula

--- scripts/rubrica2_1/functions.sql
+++ scripts/rubrica2_1/functions.sql
@@ -153,7 +153,7 @@ END;
 
 --=========================================================================
 
-CREATE OR REPLACE FUNCTION MEA_participacion_bimestre_miembro (id_part_lector number)
+CREATE OR REPLACE FUNCTION MEA_participacion_bimestre_miembro (id_part_lector number, fecha_inicial date)
 RETURN NUMBER IS
     v_inasistencias NUMBER;
     v_total_reuniones NUMBER;
@@ -162,7 +162,7 @@ BEGIN
 
     SELECT count(*) INTO v_inasistencias
         FROM mea_inasistentes
-        WHERE id_part_lector = id_lector AND fech_reunion between add_months(SYSDATE,-2) AND SYSDATE;
+        WHERE id_part_lector = id_lector AND fech_reunion between add_months(fecha_inicial,-2) AND fecha_inicial;
 
     SELECT count(*) INTO v_total_reuniones    
         FROM mea_historico_grupos h, mea_reuniones_calendario r 
@@ -172,7 +172,7 @@ BEGIN
             AND h.id_grupo = r.id_grupo 
             AND r.fech_reunion >= h.fech_i_hist_grupo 
             AND (h.fech_f_hist_grupo IS NULL OR r.fech_reunion <= h.fech_f_hist_grupo) 
-            AND r.fech_reunion between add_months(SYSDATE,-2) AND SYSDATE 
+            AND r.fech_reunion between add_months(fecha_inicial,-2) AND fecha_inicial
             AND r.realizada = 'SI';
 
     IF v_total_reuniones = 0 THEN
