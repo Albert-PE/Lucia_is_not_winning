b6ff034cd10c4f9893df9b730eafafcbb3fe609b
Alberto Rodríguez Villanueva <135548602+Albert-PE@users.noreply.github.com>
2026-06-09 01:46:54 -0400

    Limpiando Indexes

--- scripts/rubrica2_1/indexes.sql
+++ scripts/rubrica2_1/indexes.sql
@@ -1,33 +1,17 @@
 --=====================================================================
---                     ÍNDICES ESTRATÉGICOS (CRITERIOS LUCÍA)
--- 1. Tablas No Volátiles o Poco Volátiles (Maestras)
--- 2. Filtro Frecuente
--- 3. Selectividad < 2%
+--                            ÍNDICES
 --=====================================================================
 
--- 1. Búsqueda de Países por Nombre
--- Tabla: PAISES (No volátil)
--- Selectividad: ~0.5% (1 entre 200 países)
+
 CREATE INDEX idx_paises_nom ON PAISES(nom_pais);
 
--- 2. Búsqueda de Autores por Apellido
--- Tabla: AUTORES (Poco volátil)
--- Selectividad: < 1% en una base de datos extensa de autores
 CREATE INDEX idx_autores_apellido ON AUTORES(p_apellido);
 
--- 3. Búsqueda de Instituciones por Nombre
--- Tabla: INSTITUCIONES (Poco volátil)
--- Selectividad: < 2% (Un club busca su institución específica)
 CREATE INDEX idx_inst_nombre ON INSTITUCIONES(nom_inst);
 
--- 4. Búsqueda de Libros por Título
--- Tabla: LIBROS (Poco volátil - Tabla Maestra)
--- Selectividad: Muy alta (Un título específico es una fracción mínima)
 CREATE INDEX idx_libros_titulo ON LIBROS(titulo_libro);
 
 CREATE INDEX idx_fk_club_ciudades ON CIUDADES(id_pais);
---=================================================================
--- indexes para fks que no forman parte de pks
 
 CREATE INDEX idx_fk_clubes_inst ON CLUBES(id_inst);
 
