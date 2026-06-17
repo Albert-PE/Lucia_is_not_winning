971f134994b63b57bdc73c1d7b60cdc7872e8ecb
Alberto Rodríguez Villanueva <135548602+Albert-PE@users.noreply.github.com>
2026-06-17 10:23:57 -0400

    Update menu_principal.sql

--- scripts/extras/menu_principal.sql
+++ scripts/extras/menu_principal.sql
@@ -13,6 +13,9 @@ PROMPT 4. Gestionar Miembros
 PROMPT 5. Salir del Sistema
 PROMPT
 
+-- Pre-definimos la variable para evitar que SQL Developer muestre un popup pidiendo su valor
+DEFINE v_script_principal = 'menu_principal.sql'
+
 COLUMN script_to_run NEW_VALUE v_script_principal
 -- Usamos & directo para que SQL Developer abra el popup automáticamente
 SELECT CASE '&opcion_menu_principal'
@@ -25,3 +28,6 @@ SELECT CASE '&opcion_menu_principal'
 END AS script_to_run FROM DUAL;
 
 @@&v_script_principal
+
+-- Limpiamos la variable de opcion para que la vuelva a pedir la proxima vez
+UNDEFINE opcion_menu_principal
